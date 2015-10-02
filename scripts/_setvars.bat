bda5c56127b4ff55d2acecd2b783d713a03191ac
Thomas Gläßle <t_glaessle@gmx.de>
2015-10-02 16:17:07 +0200

    Fix _setvars path algorithm

--- scripts/_setvars.bat
+++ scripts/_setvars.bat
@@ -1,17 +1,18 @@
 rem Adapt these pathes to your runtime:
 set _PYTHON=C:\Python27
+for %%a in (%~dp0..) do set _BASE=%%~fa
 
 rem Add python to PATH:
 set PATH=%PATH%;%_PYTHON%
 set PATH=%PATH%;%_PYTHON%\Scripts
 
 rem Make sure BeamOptikDLL.dll can be found later on:
-set PATH=%PATH%;%CD%
+set PATH=%PATH%;%_BASE%
 
 rem Add portable-lib to PYTHONPATH, so sitecustomize.py will
 rem automatically be executed whenever a python interpreter is fired:
 if DEFINED PYTHONPATH (
-    set PYTHONPATH=%PYTHONPATH%;%CD%\libs
+    set PYTHONPATH=%PYTHONPATH%;%_BASE%\libs
 ) ELSE (
-    set PYTHONPATH=%CD%\libs
+    set PYTHONPATH=%_BASE%\libs
 )
