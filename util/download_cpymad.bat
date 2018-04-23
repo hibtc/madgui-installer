f716a837e5cf3abc048000d9e836f87499510921
Thomas Gläßle <t_glaessle@gmx.de>
2018-04-23 22:37:38 +0200

    Update to latest madgui version

--- util/download_cpymad.bat
+++ util/download_cpymad.bat
@@ -5,7 +5,8 @@ if not exist "%PY_LIB%" ( mkdir "%PY_LIB%" )
 if not exist "%PY_PIP%" ( mkdir "%PY_PIP%" )
 echo on
 
-pip download -d "%PY_PIP%" cpymad
-pip install --target "%PY_LIB%" --find-links "%PY_PIP%" cpymad
+pip uninstall -y cpymad
+pip download --pre -d "%PY_PIP%" cpymad
+pip install --pre --target "%PY_LIB%" --find-links "%PY_PIP%" cpymad
 
 @pause
