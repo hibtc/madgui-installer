817903c1e4a7903984890d180d17e620a5ccc7f8
Thomas Gläßle <t_glaessle@gmx.de>
2018-05-15 00:28:02 +0200

    Cosmetic changes to _setvars.bat

--- lib/_setvars.bat
+++ lib/_setvars.bat
@@ -1,21 +1,19 @@
 @echo off
-:: This script is called from other scripts to activate the python environment.
-
-:: First load user custom config
-if exist "%~dp0..\env.bat" call "%~dp0..\env.bat"
+:: This script is called from other scripts to prepare the environment.
 
 :: Determine path of parent folder:
 for %%a in (%~dp0..) do set "MADGUI_ROOT=%%~fa"
 
-
-:: Make sure BeamOptikDLL.dll and other scripts can be found later on:
-set "PATH=%MADGUI_ROOT%\bin;%PATH%"
+:: Load user custom config
+if exist "%~dp0..\env.bat" call "%~dp0..\env.bat"
 
 :: Depend on python version/architecture
 set "PY_SRC=%MADGUI_ROOT%\src"
 set "PY_LIB=%MADGUI_ROOT%\lib\site-packages"
 set "PY_PIP=%MADGUI_ROOT%\lib\wheelhouse"
 
+:: Make sure BeamOptikDLL.dll and other scripts can be found later on:
+set "PATH=%MADGUI_ROOT%\bin;%PATH%"
 
 :: Add 'lib' folder to PYTHONPATH, so sitecustomize.py will automatically
 :: be imported whenever a python interpreter is fired:
