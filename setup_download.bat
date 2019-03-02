512ac43d5d438a9e69c80224462334e2426ede88
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-02 04:32:39 +0100

    Use `pip wheel` to prepare packages
    While this adds `wheel` as build dependency, it may significantly reduce
    the installation complexity and improve performance.

--- setup_download.bat
+++ setup_download.bat
@@ -1,5 +1,5 @@
 @call "%~dp0\activate.bat"
 
 :: Download everything (can be used for offline installation later):
-pip download -d "%PY_PIP%" ^
+pip wheel -w "%PY_PIP%" ^
     -r "%MADGUI_ROOT%\requirements.txt"
