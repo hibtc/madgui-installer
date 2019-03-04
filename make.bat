1138ebda64deb0db5bbdeb1f07a0a4805947887c
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-04 03:34:18 +0100

    Remove deprecated conda flag '--force'

--- make.bat
+++ make.bat
@@ -1,12 +1,12 @@
 :: Install py34 for mingwpy:
-call conda create -p py34 -qyf python=3.4
+call conda create -p py34 -qy python=3.4
 call conda activate .\py34
-call conda install -qyf mingwpy -c conda-forge
+call conda install -qy mingwpy -c conda-forge
 
 :: Install py37 for site-packages:
-call conda create -p py37 -qyf python=3.7 wheel
+call conda create -p py37 -qy python=3.7 wheel
 call conda activate .\py37
-call conda install -qyf nsis -c nsis
+call conda install -qy nsis -c nsis
 call pip wheel -w "%~dp0\wheelhouse" -r "%~dp0\requirements.txt"
 call pip install -t "%~dp0\site-packages" -r "%~dp0\requirements.txt" ^
     -f "%~dp0\wheelhouse" --no-index
