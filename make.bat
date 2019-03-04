17dd36334cee5f8e354f7127fb53d0e58e471dcb
Thomas Gläßle <t_glaessle@gmx.de>
2019-03-04 02:24:22 +0100

    Use local conda environments

--- make.bat
+++ make.bat
@@ -1,23 +1,18 @@
 :: Install py34 for mingwpy:
-call conda create -n py34_build -qyf python=3.4
-call conda activate py34_build
+call conda create -p py34 -qyf python=3.4
+call conda activate .\py34
 call conda install -qyf mingwpy -c conda-forge
-for /f %%G in ('python -c "import sys; print(sys.prefix)"') do (
-    set "gcc=%%~fG\Scripts\gcc.exe"
-)
 
 :: Install py37 for site-packages:
-call conda create -n py37_build -qyf python=3.7 wheel
-call conda activate py37_build
+call conda create -p py37 -qyf python=3.7 wheel
+call conda activate .\py37
 call conda install -qyf nsis -c nsis
 call pip wheel -w "%~dp0\wheelhouse" -r "%~dp0\requirements.txt"
 call pip install -t "%~dp0\site-packages" -r "%~dp0\requirements.txt" ^
     -f "%~dp0\wheelhouse" --no-index
 
-for /f %%G in ('python -c "import sys; print(sys.prefix)"') do (
-    set "pythondir=%%~fG"
-)
-
+set "gcc=py34\Scripts\gcc.exe"
+set "pythondir=py37"
 call %gcc% -o python.exe ^
     python.c ^
     -I%pythondir%\include ^
