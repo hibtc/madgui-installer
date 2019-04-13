6112fa22aa2459507cd31a9825c2d4a2d77dac88
Thomas Gläßle <t_glaessle@gmx.de>
2019-04-13 03:40:02 +0200

    Update to madgui 19.4.1

--- version.h
+++ version.h
@@ -1,6 +1,6 @@
 // defines for .rc and .nsi files:
-#define VERSION_DOT             19.3.2
-#define VERSION_COMMA           19,3,2
+#define VERSION_DOT             19.4.1
+#define VERSION_COMMA           19,4,1
 #define COPYRIGHT               "HIT GmbH"
 
 #ifdef SHOW_VERSION
