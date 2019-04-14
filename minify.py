301989d24f5b43b1e9e3149e0ab5c3826bb4bf68
Thomas Gläßle <t_glaessle@gmx.de>
2019-04-14 14:31:11 +0200

    Trash files instead of fully deleting them

--- minify.py
+++ minify.py
@@ -16,11 +16,28 @@ import py_compile
 def main(site_dirs):
     for base in site_dirs:
         for py_file in glob(base + r'\**\*.py', recursive=True):
-            if py_compile.compile(py_file, py_file + 'c'):
-                os.remove(py_file)
+            py_compile.compile(py_file, py_file + 'c')
 
-        for cachedir in glob(base + r'\**\__pycache__', recursive=True):
-            shutil.rmtree(cachedir)
+        trash(glob(base + r'\**\*.py', recursive=True))
+        trash(glob(base + r'\**\__pycache__', recursive=True))
+
+
+def trash(pathes, base='.', trash_dir='trash'):
+    for path in pathes:
+        if trash_dir is None:
+            shutil.rmtree(path)
+        elif os.path.isfile(path):
+            dest = os.path.join(trash_dir, os.path.relpath(path, base))
+            os.makedirs(os.path.dirname(dest), exist_ok=True)
+            os.rename(path, dest)
+        elif os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
+                dest = os.path.join(trash_dir, os.path.relpath(root, base))
+                os.makedirs(dest, exist_ok=True)
+                for file in files:
+                    os.rename(
+                        os.path.join(root, file),
+                        os.path.join(dest, file))
 
 
 if __name__ == '__main__':
