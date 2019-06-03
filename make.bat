5844e69cd96239b808c35f1c0eccff59574ca46f
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-03 12:40:13 +0200

    Build with MSVC

--- make.bat
+++ make.bat
@@ -10,16 +10,12 @@ set "URL=https://www.python.org/ftp/python/3.7.3/%ZIP%"
 call python -m wget "%URL%" -o "%ZIP%"                          || goto :error
 call 7za x -y -opkg "%ZIP%"                                     || goto :error
 
-:: Install py34 for mingwpy:
-call conda create -p py34 -qy python=3.4                        || goto :error
-call conda install -p py34 -qy mingwpy -c conda-forge           || goto :error
-
-set "gcc=py34\Scripts\gcc.exe"
-set "windres=py34\Scripts\windres.exe"
-set "cflags=-Ipy37\include"
+set "cflags=/Ipy37\include"
 set "lflags=-Lpy37\libs -lpython37"
 
-call %gcc% %cflags% python.c %lflags% -o pkg\python.exe         || goto :error
+
+call lib /machine:x64 /def:python.def /out:python37.lib
+call cl %cflags% python.c python37.lib /link out:pkg\python.exe         || goto :error
 
 @exit /b 0
 
