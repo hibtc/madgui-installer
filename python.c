5833b5b1a6cbf47eecfdb22dff9eeb9644f7e0f4
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-03 10:37:58 +0200

    No wWinMain

--- python.c
+++ python.c
@@ -5,10 +5,10 @@
 #define WIN32_LEAN_AND_MEAN
 #include <windows.h>
 
-int WINAPI wWinMain(
+int WINAPI WinMain(
     HINSTANCE hInstance,      /* handle to current instance */
     HINSTANCE hPrevInstance,  /* handle to previous instance */
-    LPWSTR lpCmdLine,         /* pointer to command line */
+    LPSTR lpCmdLine,         /* pointer to command line */
     int nCmdShow              /* show state of window */
 )
 {
