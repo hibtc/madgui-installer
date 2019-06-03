8cae6a650477278ac25da6d2c14d1378ac718016
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-03 14:29:10 +0200

    no python

--- make.bat
+++ make.bat
@@ -1,19 +1,9 @@
 :: Install py37 for site-packages:
-call conda create -p py37 -qy python=3.7
-call conda activate .\py37                                      || goto :error
-call conda install -qy 7za pywget                               || goto :error
 
-:: Download embeddable python:
-set "ZIP=python-3.7.3-embed-amd64.zip"
-set "URL=https://www.python.org/ftp/python/3.7.3/%ZIP%"
-call python -m wget "%URL%" -o "%ZIP%"                          || goto :error
-call 7za x -y -opkg "%ZIP%"                                     || goto :error
-
-
-call lib /machine:x64 /def:python.def /out:python37.lib
-call cl /nologo /Gm- /GR- /EHa- /Oi                              ^
-    /DUNICODE /D_UNICODE /D__UNICODE python.c                   ^
-    python37.lib kernel32.lib shell32.lib                       ^
+call md pkg
+call cl                         ^
+    python.c                            ^
+    kernel32.lib                        ^
     /link /nodefaultlib /subsystem:windows /out:pkg\python.exe                                   || goto :error
 
 @exit /b 0
--- python.c
+++ python.c
@@ -1,29 +1,17 @@
-// This file defines a simple replacement python.exe as drop in replacement for
-// the official python.exe. The reason we need this is that for some reason the
-// BeamOptikDLL64.dll does not work with the official python.exe on python 3.5
-// and later.
 #include <windows.h>
-
-extern int Py_Main(int argc, wchar_t **argv);
 
 typedef void (__stdcall * TGetInterfaceInstance) (int*, int*);
 
 void WINAPI WinMainCRTStartup()
 {
-    int argc;
-    wchar_t** wargv = CommandLineToArgvW(
-        GetCommandLineW(), &argc);
-
     HMODULE dll = LoadLibraryA("BeamOptikDLL64.dll");
 
     int iid, done;
 
-    TGetInterfaceInstance getiinst =
+    TGetInterfaceInstance GetInterfaceInstance =
         (TGetInterfaceInstance) GetProcAddress(dll, "GetInterfaceInstance");
 
-    getiinst(&iid, &done);
+    GetInterfaceInstance(&iid, &done);
 
     ExitProcess(done);
-
-    ExitProcess(Py_Main(argc, wargv));
 }
