7ef9b7ccb6bed395a4904bd35bfee30ed45289d6
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-03 10:36:58 +0200

    Strip all extraneous code

--- make.bat
+++ make.bat
@@ -9,27 +9,6 @@ set "ZIP=python-3.7.3-embed-amd64.zip"
 set "URL=https://www.python.org/ftp/python/3.7.3/%ZIP%"
 call python -m wget "%URL%" -o "%ZIP%"                          || goto :error
 call 7za x -y -opkg "%ZIP%"                                     || goto :error
-:: DO NOT add quotes or space around `import site`, it will mess up the output!
-echo import site>>pkg\python37._pth
-echo purelib.zip>>pkg\python37._pth
-
-call pip wheel -w wheels -r requirements.txt                    || goto :error
-call pip install -f wheels -r requirements.txt ^
-    -t pkg\Lib\site-packages --no-index -I                      || goto :error
-call rd /s /q pkg\Lib\site-packages\bin
-
-:: Safe our packages at top level to make them easier to find and edit,
-:: and to prevent them from being minified down below:
-call move pkg\Lib\site-packages\madgui pkg\
-call move pkg\Lib\site-packages\minrpc pkg\
-call move pkg\Lib\site-packages\cpymad pkg\
-call move pkg\Lib\site-packages\hit_acs pkg\
-
-:: Performance tweaks: remove .py files and zip up pure thirdparty packages:
-call pip install distlib                                        || goto :error
-call python minify.py pkg\Lib\site-packages                     || goto :error
-call python purelib.py pkg\Lib\site-packages                    || goto :error
-call move purelib.zip pkg
 
 :: Install py34 for mingwpy:
 call conda create -p py34 -qy python=3.4                        || goto :error
@@ -40,23 +19,8 @@ set "windres=py34\Scripts\windres.exe"
 set "cflags=-Ipy37\include"
 set "lflags=-Lpy37\libs -lpython37"
 
-:: Determine madgui version, and create madgui.rc:
-for /f %%G in ('pkg\python -c "import madgui; print(madgui.__version__)"') do (set "VERSION=%%G")
-call pip install j2cli
-call j2 madgui.template.rc > madgui.rc
-
 call %gcc% %cflags% python.c %lflags% -o pkg\python.exe         || goto :error
 
-call %windres% madgui.rc -O coff -o madgui.res                  || goto :error
-call %gcc% %cflags% launcher.c %lflags% -o pkg\madgui.exe ^
-    -DMODULE=madgui madgui.res -mwindows                        || goto :error
-call %gcc% %cflags% launcher.c %lflags% -o pkg\beamopt.exe ^
-    -DMODULE=hit_acs.gui_qt                                     || goto :error
-call %gcc% %cflags% shim.c %lflags% -o madgui.exe ^
-    -DEXE=madgui_%VERSION%\\madgui.exe madgui.res -mwindows      || goto :error
-
-copy madgui.yml pkg                                             || goto :error
-call makensis /DVERSION=%VERSION% madgui.nsi                    || goto :error
 @exit /b 0
 
 :error
