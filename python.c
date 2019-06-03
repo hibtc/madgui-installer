9bf357aea4e03db1f440519ff4f3087b122b9ce4
Thomas Gläßle <t_glaessle@gmx.de>
2019-06-03 14:01:43 +0200

    tchar.h

--- python.c
+++ python.c
@@ -2,7 +2,7 @@
 // the official python.exe. The reason we need this is that for some reason the
 // BeamOptikDLL64.dll does not work with the official python.exe on python 3.5
 // and later.
-#define WIN32_LEAN_AND_MEAN
+#include "tchar.h"
 #include <windows.h>
 
 extern int Py_Main(int argc, wchar_t **argv);
